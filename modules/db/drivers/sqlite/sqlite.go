3a297f0142d36f08da3c6903a06c54d4fe7e6f2a
ivanhao <ivanhao1984@qq.com>
2023-11-26 21:11:42 +0800

    Update sqlite.go

--- modules/db/drivers/sqlite/sqlite.go
+++ modules/db/drivers/sqlite/sqlite.go
@@ -1,3 +1,4 @@
 package sqlite
 
-import _ "github.com/mattn/go-sqlite3" // Import the sqlite driver.
+//import _ "github.com/mattn/go-sqlite3" // Import the sqlite driver.
+import _ "github.com/ivanhao/go-sqlite3" //new
